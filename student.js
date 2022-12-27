bbe8d2e22f20a1a1eb6ec13672b6d91a81690e6c
kaushik134 <kaushikvaghasiya134@gmail.com>
2022-12-27 15:16:05 +0530

    task3

---
+++ student.js
@@ -0,0 +1,12 @@
+db.student.insertMany([
+    {id:1,name:"kaushik",class:"tybca",CGPA:7.91},
+    {id:2,name:"bhautik",class:"sybca",CGPA:5.51},
+    {id:3,name:"smit",class:"tyBsc IT",CGPA:6.91},
+    {id:4,name:"jaimin",class:"tybca",CGPA:7.32},
+    {id:5,name:"dhruvit",class:"finance",country:"usa"},
+    {id:6,name:"dhruvik",class:"tybca",CGPA:6.43},
+    {id:7,name:"harshil",class:"tybcom",CGPA:"fail",study:"flutter"},
+    {id:8,name:"het",class:"11 commerce",percentage:60},
+    {id:9,name:"vaidehi",class:"sybba",CGPA:7.91},
+    {id:10,name:"dehuti",class:"ba with phycology",CGPA:8.87}
+])
